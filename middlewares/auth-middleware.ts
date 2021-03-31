0e7f0e857c2d4f47f97bd1d14a8ddd7cc807d328
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2021-03-31 17:37:54 +0200

    [REFACTO ACCES]

--- middlewares/auth-middleware.ts
+++ middlewares/auth-middleware.ts
@@ -13,28 +13,29 @@ import {CLIENT_USER_TYPE_ID} from "../consts";
  */
 export async function authOtherThanClientMiddleWare(req: express.Request, res: express.Response, next: express.NextFunction) {
     const auth = req.headers['authorization'];
-    if(auth !== undefined) {
+    if (auth !== undefined) {
         //récupération du token autorisé
         const token = auth.slice(7);
         const connection = await DatabaseUtils.getConnection();
         const sessionController = new SessionController(connection);
         const userController = new UserController(connection);
         const session = await sessionController.getSessionByToken(token);
-        if(session !== null) {
+        if (session !== null) {
+            //contrôle des droits d'accès
             if (session.userId != null) {
                 const user = await userController.getUserById(session.userId);
-                if(user?.typeId === CLIENT_USER_TYPE_ID || user?.typeId === undefined || user.typeId === null) {
-                    //accès refusé aux clients ou aux utilisateurs sans type
-                    res.status(403).end();
+                if (user?.typeId !== CLIENT_USER_TYPE_ID && user?.typeId !== undefined) {
+                    next();
                     return;
                 }
             }
-            next();
-            return;
-        } else {
-            //pas le droit d'y accéder -> aucune session associée à ce token
-            res.status(403).end();
         }
+        /* pas le droit d'y accéder si
+           -> Aucune session associée au token
+           -> Aucun utilisateur associé à la session
+           -> Utilisateur de type client ou sans type
+         */
+        res.status(403).end();
     } else {
         //pas d'user connecté
         res.status(401).end();
