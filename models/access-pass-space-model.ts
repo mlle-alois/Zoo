332789c9b46b1410fa83fe26a30a9400c5e98b9a
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2021-04-14 19:51:54 +0200

    [Z026]Automatisation de l'ordre des accès aux espaces pour les billets escape game

--- models/access-pass-space-model.ts
+++ models/access-pass-space-model.ts
@@ -1,17 +1,17 @@
 export interface IAccessPassSpaceModelProps {
     passTypeId: number;
     spaceId: number;
-    numOrderAccess: number;
+    numOrderAccess?: number;
 }
 
 export class AccessPassSpaceModel implements IAccessPassSpaceModelProps {
     passTypeId: number;
     spaceId: number;
-    numOrderAccess: number;
+    numOrderAccess?: number;
 
     constructor(props: IAccessPassSpaceModelProps) {
         this.passTypeId = props.passTypeId;
         this.spaceId= props.spaceId;
-        this.numOrderAccess = props.numOrderAccess;
+        this.numOrderAccess = props?.numOrderAccess;
     }
 }
